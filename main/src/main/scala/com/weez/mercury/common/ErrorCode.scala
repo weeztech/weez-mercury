d0aea0b22479c5dc1a00b61015c0c46f676b5e0c
niuhaifeng <niuhf0452@gmail.com>
2014-12-09 15:06:43 +0800

    none

---
+++ main/src/main/scala/com/weez/mercury/common/ErrorCode.scala
@@ -0,0 +1,13 @@
+package com.weez.mercury.common
+
+import spray.json._
+
+case class ErrorCode(code: Int, message: String)
+
+object ErrorCode {
+  val InvalidRequest = ErrorCode(800, "the request contains characters cannot be parsed or has invalid format")
+}
+
+case class ServerResponse(
+
+                           )
