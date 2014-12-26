184da29096d2dce6619fab06fa294e9d8d61e527
niuhaifeng <niuhf0452@gmail.com>
2014-12-26 17:35:41 +0800

    none

--- main/src/main/scala/com/weez/mercury/common/DB.scala
+++ main/src/main/scala/com/weez/mercury/common/DB.scala
@@ -4,14 +4,4 @@ import scala.slick.driver._
 
 object DB {
   val driver = MySQLDriver
-
-  import driver.simple._
-
-  val fulltextMatch = SimpleExpression.binary[String, String, Boolean] { (col, search, qb) =>
-    qb.sqlBuilder += "match("
-    qb.expr(col)
-    qb.sqlBuilder += ") against ("
-    qb.expr(search)
-    qb.sqlBuilder += " in boolean mode)"
-  }
 }
