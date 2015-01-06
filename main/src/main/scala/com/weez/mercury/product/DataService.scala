6187258d225466efd6008141ccea7bb968ac287b
gaojingxin <gao_jing_xin@126.com>
2015-01-06 18:22:42 +0800

    changes

--- main/src/main/scala/com/weez/mercury/product/DataService.scala
+++ main/src/main/scala/com/weez/mercury/product/DataService.scala
@@ -2,11 +2,13 @@ package com.weez.mercury.product
 
 import com.weez.mercury.common._
 
+import scala.slick.jdbc.SetParameter
+
 object DataService extends RemoteService {
   def availableAssistants: QueryCall = c => {
     import c._
     val items = sql"""SELECT a.id,s.name,a.price,a.description
-             FROM biz_assistant a JOIN biz_staffs s on s.id = a.id
+             FROM biz_assistant a JOIN biz_staffs s ON s.id = a.id
          """.as[ModelObject].list
     completeWith("items" -> items)
   }
@@ -21,7 +23,7 @@ object DataService extends RemoteService {
     import c._
     def getProducts(keywords: String, start: Int, count: Int) = {
       sql"""SELECT id,code,title,price,description FROM biz_product_models
-           where title like CONCAT('%',$keywords,'%') LIMIT $start,$count""".as[ModelObject]
+           WHERE title LIKE CONCAT('%',$keywords,'%') LIMIT $start,$count""".as[ModelObject]
     }
     completeWith("items" -> getProducts(request.keywords, request.start, request.count).list)
   }
