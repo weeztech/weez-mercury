4fe3d89ceabb36685da7540d4807a534b2e74971
gaojingxin <gao_jing_xin@126.com>
2015-01-09 12:52:35 +0800

    changes

--- main/src/main/scala/com/weez/mercury/product/DataService.scala
+++ main/src/main/scala/com/weez/mercury/product/DataService.scala
@@ -28,26 +28,81 @@ object DataService extends RemoteService {
   }
 }
 
+case class Product(code: String,
+                   title: String,
+                   description: String,
+                   price: Double)
 
-case class Customer()
+object Product extends DBObjectType[Product] {
+  def nameInDB = "product"
 
-case class SaleOrder(code: String, time: DateTime, customer: Ref[Customer], rooms: KeyCollection[SaleOrder.RoomItem], ctime: DateTime)
+  val code = column[String]("code")
+
+  val title = column[String]("title")
+}
+
+object ProductCollection extends RootCollection[Product] {
+  val byCode = defUniqueIndex("by-code", Product.code)
+}
+
+
+
+case class Assistant(code: String,
+                     title: String)
+
+object Assistant extends DBObjectType[Assistant] {
+  def nameInDB = "assistant"
+
+  val code = column[String]("code")
+
+  val title = column[String]("title")
+}
+
+object AssistantCollection extends RootCollection[Assistant] {
+  val byCode = defUniqueIndex("by-code", Assistant.code)
+}
+
+
+case class Customer(code: String,
+                    title: String)
+
+object Customer extends DBObjectType[Customer] {
+  def nameInDB = "customer"
+
+  val code = column[String]("code")
+
+  val title = column[String]("title")
+}
+
+object CustomerCollection extends RootCollection[Customer] {
+  val byCode = defUniqueIndex("by-code", Customer.code)
+}
+
+case class SaleOrder(code: String,
+                     time: DateTime,
+                     customer: Ref[Customer],
+                     rooms: KeyCollection[SaleOrder.RoomItem],
+                     ctime: DateTime)
 
 object SaleOrder extends DBObjectType[SaleOrder] {
   def nameInDB = "sale-order"
 
-  def code = column[String]("code")
+  val code = column[String]("code")
 
-  def dept = column[Dept]("dept")
+  val customer = column[Ref[Customer]]("code")
 
-  def access = column[KeyCollection[Dept]]("access")
+  val time = column[DateTime]("time")
+
+  val ctime = column[DateTime]("ctime")
+
+  val rooms = column[KeyCollection[RoomItem]]("rooms")
 
   case class RoomItem(room: Ref[Room], startTime: DateTime, endTime: DateTime)
 
 }
 
 object SaleOrderCollection extends RootCollection[SaleOrder] {
-  def byUsername = defUniqueIndex("by-username", User.name)
+  val byCode = defUniqueIndex("by-code", SaleOrder.code)
 }
 
 case class Room(code: String)
